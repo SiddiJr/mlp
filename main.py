2a4cac826add3d834ec730d81585af5b52c2bbb4
sidjr <sidnei_1998@hotmail.com>
2023-12-06 21:16:32 -0300

    predicts certos e salvando

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error
 import pandas as pd
+import csv
 
 #cria dataframe "tabela" que guarda a separação de X e y de treino e teste.
 X_train = pd.DataFrame()
 y_train = pd.DataFrame()
 X_test = pd.DataFrame()
 y_test = pd.DataFrame()
+mse = 0
 
 #importa dataset
 col_names = ['ID', 'p_Sist', 'p_Diast','qualPres', 'pulso', 'resp', 'gravidade', 'clas_grav']
-df = pd.read_csv('datasets/data_20x20_42vic/sinais_vitais.txt', names = col_names, index_col=False)
+df = pd.read_csv('datasets\data_800vic\sinais_vitais_com_label.txt', names = col_names, index_col=False)
 df = df[['ID', 'qualPres', 'pulso', 'resp', 'gravidade']]
 X = df[['qualPres', 'pulso', 'resp']]
 y = df[['gravidade']]
 
 #tamanho do batch e número de folds
-batch_Size = 5
+batch_Size = 200
 k_num = 10
 
 #cria o kfold com k_num folds e aleatorizando o dataset
@@ -28,11 +30,12 @@ kf = KFold(n_splits = k_num, shuffle = True)
 #número máximo de iterações(condição de parada) 100, warm_start(guarda os 'fits' anteriores) true.
 mlp = MLPRegressor(solver='sgd',
                    batch_size = batch_Size,
-                   hidden_layer_sizes=(3,3,3),
+                   hidden_layer_sizes=(4,4),
                    learning_rate_init = 0.0007,
-                   momentum=0.7,
-                   max_iter=100,
-                   warm_start=True)
+                   momentum=0.9,
+                   max_iter=700,
+                   warm_start=True,
+                   activation='logistic')
 
 #separa o dataset df.
 for train_index, test_index in kf.split(df):
@@ -60,7 +63,11 @@ for train_index, test_index in kf.split(df):
     y_pred = mlp.predict(X_test)
 
     #erro médio quadrático.
-    mse = mean_squared_error(y_test, y_pred)
+    mse = mse + mean_squared_error(y_test, y_pred)
 
-    #print :)
-    print(mse)
+#print :)
+print(mse / k_num)
+
+with open('a.csv', 'a+', newline='') as f:
+    writer_object = csv.writer(f)
+    writer_object.writerows([[val] for val in y_pred])
